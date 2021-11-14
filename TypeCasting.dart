23c4130a0223080c73f3cdc6e2dc85944b9dd2c3
Anand <iamtheanandsingh@gmail.com>
2021-11-14 17:03:52 +0530

    14112021

--- TypeCasting.dart
+++ TypeCasting.dart
@@ -1,10 +1,9 @@
-import 'dart:io'
+import 'dart:io';
 
-void main()
-{
+void main() {
   int a = 10;
   int b = 20;
   stdout.writeln("A Is $a and B Is $b");
-  stdout.writeln("Sum Of A And B Is : ${a+b}");
-  print("A Is "+ a.toString());
+  stdout.writeln("Sum Of A And B Is : ${a + b}");
+  print("A Is " + a.toString());
 }
